e8a017eb381b2b871753dff2297f374d8f6e22cd
dov-yih <yidafu90@qq.com>
2019-10-30 22:34:10 +0800

    feat: [27] Remove Element

---
+++ 27.remove-element.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=27 lang=javascript
+ *
+ * [27] Remove Element
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+const removeElement = function(nums, val) {
+  let lastIdx = nums.length;
+  for (let i = 0; i < lastIdx; i++) {
+    if (nums[i] === val) {
+      lastIdx--;
+      while (i < lastIdx && nums[lastIdx] === val) {
+        lastIdx--;
+      }
+      nums[i] = nums[lastIdx];
+    }
+  }
+  return lastIdx;
+};
+// @lc code=end
