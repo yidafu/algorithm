f3964884122163a4f3f0c7199c104551eaa55e69
dov-yih <yidafu90@qq.com>
2019-10-30 22:33:46 +0800

    feat: [26] Remove Duplicates from Sorted Array

---
+++ 26.remove-duplicates-from-sorted-array.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=26 lang=javascript
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const removeDuplicates = function(nums) {
+  let len = 0;
+  let duplicate = true;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] === nums[i]) {
+        duplicate = true;
+        break;
+      }
+      duplicate = false;
+    }
+
+    if (!duplicate) {
+      // swap
+      const tmp = nums[len];
+      nums[len] = nums[i];
+      nums[i] = tmp;
+      len++;
+    }
+  }
+  return len;
+};
+// @lc code=end
