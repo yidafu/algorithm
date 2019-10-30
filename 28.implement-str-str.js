c230ea6ec3f46166438a1d9ade88ba166cc30429
dov-yih <yidafu90@qq.com>
2019-10-30 23:01:14 +0800

    feat: [28] Implement strStr()

---
+++ 28.implement-str-str.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=28 lang=javascript
+ *
+ * [28] Implement strStr()
+ */
+
+// @lc code=start
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+const strStr = function(haystack, needle) {
+  if (needle.length === 0) return 0;
+
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] === needle[0]) {
+      if (haystack.length - i < needle.length) {
+        return -1;
+      }
+
+      let j = 1;
+      for (; j < needle.length; j++) {
+        if (needle[j] !== haystack[i + j]) {
+          break;
+        }
+      }
+
+      if (j === needle.length) {
+        return i;
+      }
+    }
+  }
+  return -1;
+};
+// @lc code=end
+
+strStr('a', 'a');
